f726fa53d2e82b71fd788d1e26df3ba3c6af1d44
Mirza <msehovic92@gmail.com>
2015-12-27 16:28:10 +0100

    Added initial Emger project

---
+++ Projects/Emger/app/scripts/controllers/employees.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name emgerApp.controller:EmployeesCtrl
+ * @description
+ * # EmployeesCtrl
+ * Controller of the emgerApp
+ */
+angular.module('emgerApp')
+  .controller('EmployeesCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
